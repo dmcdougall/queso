c8df641872e657b2a9786b2b615f142167da5d43
Damon McDougall <damon@ices.utexas.edu>
2018-09-23 19:12:24 -0500

    Make sequence_of_vectors use libmesh types

--- test/unit/sequence_of_vectors.C
+++ test/unit/sequence_of_vectors.C
@@ -36,6 +36,8 @@
 #include <queso/VectorSpace.h>
 #include <queso/SequenceOfVectors.h>
 #include <queso/BoxSubset.h>
+#include <queso/GslNumericVector.h>
+#include <queso/GslSparseMatrix.h>
 
 #include <cmath>
 #include <vector>
@@ -63,11 +65,11 @@ public:
   void setUp()
   {
     env.reset(new QUESO::FullEnvironment("","",NULL));
-    space.reset(new QUESO::VectorSpace<>(*env, "", 2, NULL));
-    sequence.reset(new QUESO::SequenceOfVectors<>(*space, 13, ""));
+    space.reset(new QUESO::VectorSpace<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> >(*env, "", 2, NULL));
+    sequence.reset(new QUESO::SequenceOfVectors<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> >(*space, 13, ""));
 
     // Fill up the sequence with some not-so-random stuff
-    QUESO::GslVector v(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> v(space->zeroVector());
     for (unsigned int i = 0; i < sequence->subSequenceSize(); i++) {
       v[0] = i;
       v[1] = i + 1;
@@ -77,12 +79,12 @@ public:
 
   void test_uniformly_sampled_cdf()
   {
-    QUESO::GslVector num_points(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> num_points(space->zeroVector());
     num_points.cwSet(14.0);
 
-    QUESO::VectorSpace<> rowSpace(*env, "", sequence->subSequenceSize(), NULL);
-    QUESO::ArrayOfOneDGrids<> cdfGrids("", rowSpace);
-    QUESO::ArrayOfOneDTables<> cdfValues("", rowSpace);
+    QUESO::VectorSpace<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> > rowSpace(*env, "", sequence->subSequenceSize(), NULL);
+    QUESO::ArrayOfOneDGrids<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> > cdfGrids("", rowSpace);
+    QUESO::ArrayOfOneDTables<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> > cdfValues("", rowSpace);
     sequence->subUniformlySampledCdf(num_points, cdfGrids, cdfValues);
 
     for (unsigned int i = 0; i < sequence->subSequenceSize(); i++) {
@@ -104,11 +106,11 @@ public:
 
   void test_std_var()
   {
-    QUESO::GslVector mean(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> mean(space->zeroVector());
     mean[0] = 6.0;
     mean[1] = 7.0;
-    QUESO::GslVector std(space->zeroVector());
-    QUESO::GslVector var(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> std(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> var(space->zeroVector());
 
     double actualSampleStd = std::sqrt(182.0 / 12.0);
     sequence->subSampleStd(0, sequence->subSequenceSize(), mean, std);
@@ -135,11 +137,11 @@ public:
 
   void test_hists()
   {
-    QUESO::GslVector min(space->zeroVector());
-    QUESO::GslVector max(space->zeroVector());
-    std::vector<QUESO::GslVector *> centers(14, (QUESO::GslVector *)NULL);
-    std::vector<QUESO::GslVector *> bins1(14, (QUESO::GslVector *)NULL);
-    std::vector<QUESO::GslVector *> bins2(14, (QUESO::GslVector *)NULL);
+    QUESO::GslNumericVector<libMesh::Number> min(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> max(space->zeroVector());
+    std::vector<QUESO::GslNumericVector<libMesh::Number> *> centers(14, (QUESO::GslNumericVector<libMesh::Number> *)NULL);
+    std::vector<QUESO::GslNumericVector<libMesh::Number> *> bins1(14, (QUESO::GslNumericVector<libMesh::Number> *)NULL);
+    std::vector<QUESO::GslNumericVector<libMesh::Number> *> bins2(14, (QUESO::GslNumericVector<libMesh::Number> *)NULL);
 
     sequence->subMinMaxExtra(0, sequence->subSequenceSize(), min, max);
     sequence->subHistogram(0, min, max, centers, bins1);
@@ -161,7 +163,7 @@ public:
 
   void test_iqr()
   {
-    QUESO::GslVector iqr(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> iqr(space->zeroVector());
     sequence->subInterQuantileRange(0, iqr);
 
     double actualIqr = 7.0;
@@ -177,31 +179,31 @@ public:
 
   void test_auto_covariance()
   {
-    QUESO::GslVector actualMean(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> actualMean(space->zeroVector());
     actualMean[0] = 6.0;
     actualMean[1] = 7.0;
     double actualPopVar = 182.0 / 13.0;
     unsigned int numPos = sequence->subSequenceSize();
 
     // Compute auto covariance with zero lag.  Should be population var?
-    QUESO::GslVector autoCov(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> autoCov(space->zeroVector());
     sequence->autoCovariance(0, numPos, actualMean, 0, autoCov);
     CPPUNIT_ASSERT_EQUAL(actualPopVar, autoCov[0]);
     CPPUNIT_ASSERT_EQUAL(actualPopVar, autoCov[1]);
 
     // This is normalised by autoCov, so result should be 1.0 with lag 0
-    QUESO::GslVector autoCorrViaDef(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> autoCorrViaDef(space->zeroVector());
     sequence->autoCorrViaDef(0, numPos, 0, autoCorrViaDef);
     CPPUNIT_ASSERT_EQUAL(1.0, autoCorrViaDef[0]);
     CPPUNIT_ASSERT_EQUAL(1.0, autoCorrViaDef[1]);
 
     std::vector<unsigned int> lags(2, 0);
-    std::vector<QUESO::GslVector *> autoCorrsViaFft(2, (QUESO::GslVector *)NULL);
+    std::vector<QUESO::GslNumericVector<libMesh::Number> *> autoCorrsViaFft(2, (QUESO::GslNumericVector<libMesh::Number> *)NULL);
     sequence->autoCorrViaFft(0, numPos, lags, autoCorrsViaFft);
     CPPUNIT_ASSERT_EQUAL(1.0, (*autoCorrsViaFft[0])[0]);
     CPPUNIT_ASSERT_EQUAL(1.0, (*autoCorrsViaFft[0])[1]);
 
-    QUESO::GslVector autoCorrsViaFftSum(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> autoCorrsViaFftSum(space->zeroVector());
     sequence->autoCorrViaFft(0, numPos, 1, autoCorrsViaFftSum);
     CPPUNIT_ASSERT_EQUAL(1.0, autoCorrsViaFftSum[0]);
     CPPUNIT_ASSERT_EQUAL(1.0, autoCorrsViaFftSum[1]);
@@ -209,7 +211,7 @@ public:
 
   void test_read()
   {
-    QUESO::SequenceOfVectors<> read_sequence(*space, 5, "");
+    QUESO::SequenceOfVectors<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> > read_sequence(*space, 5, "");
 
     // Filename logic
     std::string sequenceFileName = "unit/read_vector_sequence";
@@ -220,7 +222,7 @@ public:
 
     read_sequence.unifiedReadContents(sequenceFileName, "m", 5);
 
-    QUESO::GslVector value(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> value(space->zeroVector());
     for (unsigned int i = 0; i < 5; i++) {
       double val1 = i + 1;
       double val2 = i + 2;
@@ -233,13 +235,13 @@ public:
   void test_scale_kde()
   {
     double TOL = 1e-12;
-    QUESO::GslVector actualIrq(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> actualIrq(space->zeroVector());
     actualIrq.cwSet(7.0);
 
     // Regression solution
     double actualKDEScale = 2.471509395452762269940194528317;
 
-    QUESO::GslVector KDEScale(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> KDEScale(space->zeroVector());
     sequence->subScalesForKde(0, actualIrq, 1, KDEScale);
     CPPUNIT_ASSERT_DOUBLES_EQUAL(actualKDEScale, KDEScale[0], TOL);
     CPPUNIT_ASSERT_DOUBLES_EQUAL(actualKDEScale, KDEScale[1], TOL);
@@ -253,15 +255,15 @@ public:
   {
     double TOL = 1e-12;
 
-    QUESO::SequenceOfVectors<> point(*space, 1, "");
-    QUESO::GslVector mp1(space->zeroVector());
+    QUESO::SequenceOfVectors<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> > point(*space, 1, "");
+    QUESO::GslNumericVector<libMesh::Number> mp1(space->zeroVector());
     mp1[0] = -0.1;
     mp1[1] = -0.1;
     point.setPositionValues(0, mp1);
 
-    std::vector<QUESO::GslVector *> positions(1, &mp1);
-    std::vector<QUESO::GslVector *> density(1, (QUESO::GslVector *)NULL);
-    QUESO::GslVector scale(space->zeroVector());
+    std::vector<QUESO::GslNumericVector<libMesh::Number> *> positions(1, &mp1);
+    std::vector<QUESO::GslNumericVector<libMesh::Number> *> density(1, (QUESO::GslNumericVector<libMesh::Number> *)NULL);
+    QUESO::GslNumericVector<libMesh::Number> scale(space->zeroVector());
     scale.cwSet(1.0);
     point.subGaussian1dKde(0, scale, positions, density);
 
@@ -276,8 +278,8 @@ public:
 
 private:
   typename QUESO::ScopedPtr<QUESO::BaseEnvironment>::Type env;
-  typename QUESO::ScopedPtr<QUESO::VectorSpace<> >::Type space;
-  typename QUESO::ScopedPtr<QUESO::SequenceOfVectors<> >::Type sequence;
+  typename QUESO::ScopedPtr<QUESO::VectorSpace<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> > >::Type space;
+  typename QUESO::ScopedPtr<QUESO::SequenceOfVectors<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> > >::Type sequence;
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SequenceOfVectorsTest);
