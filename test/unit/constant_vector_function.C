ba22fac80c9edaefdc311861f5d823e90f8c3982
Damon McDougall <damon@ices.utexas.edu>
2018-09-23 19:00:09 -0500

    Make constant_vector_function use libmesh types

--- test/unit/constant_vector_function.C
+++ test/unit/constant_vector_function.C
@@ -34,6 +34,8 @@
 #include <queso/VectorSpace.h>
 #include <queso/BoxSubset.h>
 #include <queso/ConstantVectorFunction.h>
+#include <queso/GslNumericVector.h>
+#include <queso/GslSparseMatrix.h>
 
 namespace QUESOTesting
 {
@@ -51,28 +53,28 @@ public:
   {
     env.reset(new QUESO::FullEnvironment("","",NULL));
 
-    space.reset(new QUESO::VectorSpace<>(*env, "", 1, NULL));
+    space.reset(new QUESO::VectorSpace<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> >(*env, "", 1, NULL));
 
-    min1.reset(new QUESO::GslVector(space->zeroVector()));
+    min1.reset(new QUESO::GslNumericVector<libMesh::Number>(space->zeroVector()));
     (*min1)[0] = 1;
-    max1.reset(new QUESO::GslVector(space->zeroVector()));
+    max1.reset(new QUESO::GslNumericVector<libMesh::Number>(space->zeroVector()));
     (*max1)[0] = 3;
 
-    domain.reset(new QUESO::BoxSubset<>("", *space, *min1, *max1));
-    image.reset(new QUESO::BoxSubset<>("", *space, *min1, *max1));
+    domain.reset(new QUESO::BoxSubset<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> >("", *space, *min1, *max1));
+    image.reset(new QUESO::BoxSubset<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> >("", *space, *min1, *max1));
 
-    val.reset(new QUESO::GslVector(space->zeroVector()));
+    val.reset(new QUESO::GslNumericVector<libMesh::Number>(space->zeroVector()));
     (*val)[0] = 2.0;
 
-    fn.reset(new QUESO::ConstantVectorFunction<>("", *domain, *image, *val));
+    fn.reset(new QUESO::ConstantVectorFunction<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number>, QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> >("", *domain, *image, *val));
   }
 
   void test_compute()
   {
-    QUESO::GslVector point(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> point(space->zeroVector());
     point[0] = 1.5;
 
-    QUESO::GslVector result(space->zeroVector());
+    QUESO::GslNumericVector<libMesh::Number> result(space->zeroVector());
     fn->compute(point, NULL, result, NULL, NULL, NULL);
 
     CPPUNIT_ASSERT_EQUAL(result[0], (*val)[0]);
@@ -80,13 +82,13 @@ public:
 
 private:
   typename QUESO::ScopedPtr<QUESO::BaseEnvironment>::Type env;
-  typename QUESO::ScopedPtr<QUESO::VectorSpace<> >::Type space;
-  typename QUESO::ScopedPtr<QUESO::GslVector>::Type min1;
-  typename QUESO::ScopedPtr<QUESO::GslVector>::Type max1;
-  typename QUESO::ScopedPtr<QUESO::BoxSubset<> >::Type domain;
-  typename QUESO::ScopedPtr<QUESO::BoxSubset<> >::Type image;
-  typename QUESO::ScopedPtr<QUESO::GslVector>::Type val;
-  typename QUESO::ScopedPtr<QUESO::ConstantVectorFunction<> >::Type fn;
+  typename QUESO::ScopedPtr<QUESO::VectorSpace<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> > >::Type space;
+  typename QUESO::ScopedPtr<QUESO::GslNumericVector<libMesh::Number>>::Type min1;
+  typename QUESO::ScopedPtr<QUESO::GslNumericVector<libMesh::Number>>::Type max1;
+  typename QUESO::ScopedPtr<QUESO::BoxSubset<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> > >::Type domain;
+  typename QUESO::ScopedPtr<QUESO::BoxSubset<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> > >::Type image;
+  typename QUESO::ScopedPtr<QUESO::GslNumericVector<libMesh::Number>>::Type val;
+  typename QUESO::ScopedPtr<QUESO::ConstantVectorFunction<QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number>, QUESO::GslNumericVector<libMesh::Number>, QUESO::GslSparseMatrix<libMesh::Number> > >::Type fn;
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ConstantVectorFunctionTest);
