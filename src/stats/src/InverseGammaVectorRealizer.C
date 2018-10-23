72e6ccee0b32bf79c306a66e71c52879caf7f665
Damon McDougall <damon@ices.utexas.edu>
2018-10-23 16:32:28 -0500

    Add explicit instantiation around eigen types

--- src/stats/src/InverseGammaVectorRealizer.C
+++ src/stats/src/InverseGammaVectorRealizer.C
@@ -28,6 +28,8 @@
 #include <queso/GslNumericVector.h>
 #include <queso/GslMatrix.h>
 #include <queso/GslSparseMatrix.h>
+#include <libmesh/eigen_sparse_vector.h>
+#include <libmesh/eigen_sparse_matrix.h>
 
 namespace QUESO {
 
@@ -71,5 +73,6 @@ InverseGammaVectorRealizer<V,M>::realization(V& nextValues) const
 
 template class InverseGammaVectorRealizer<GslVector, GslMatrix>;
 template class InverseGammaVectorRealizer<GslNumericVector<libMesh::Number>, GslSparseMatrix<libMesh::Number> >;
+template class InverseGammaVectorRealizer<libMesh::EigenSparseVector<libMesh::Number>, libMesh::EigenSparseMatrix<libMesh::Number> >;
 
 }  // End namespace QUESO
