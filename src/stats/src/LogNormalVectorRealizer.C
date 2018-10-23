53df8b6248786c0a350dd1c6a58c8891bdcf5926
Damon McDougall <damon@ices.utexas.edu>
2018-10-23 16:34:49 -0500

    Add explicit instantiation around eigen types

--- src/stats/src/LogNormalVectorRealizer.C
+++ src/stats/src/LogNormalVectorRealizer.C
@@ -28,6 +28,8 @@
 #include <queso/GslNumericVector.h>
 #include <queso/GslMatrix.h>
 #include <queso/GslSparseMatrix.h>
+#include <libmesh/eigen_sparse_vector.h>
+#include <libmesh/eigen_sparse_matrix.h>
 
 namespace QUESO {
 
@@ -145,5 +147,6 @@ LogNormalVectorRealizer<V,M>::realization(V& nextValues) const
 
 template class LogNormalVectorRealizer<GslVector, GslMatrix>;
 template class LogNormalVectorRealizer<GslNumericVector<libMesh::Number>, GslSparseMatrix<libMesh::Number> >;
+template class LogNormalVectorRealizer<libMesh::EigenSparseVector<libMesh::Number>, libMesh::EigenSparseMatrix<libMesh::Number> >;
 
 }  // End namespace QUESO
