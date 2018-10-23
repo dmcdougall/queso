e408d43219c9ee23e2dc3732d0567717a4015ac9
Damon McDougall <damon@ices.utexas.edu>
2018-10-23 17:20:22 -0500

    Add explicit instantiation around eigen types

--- src/stats/src/ConcatenatedJointPdf.C
+++ src/stats/src/ConcatenatedJointPdf.C
@@ -28,6 +28,8 @@
 #include <queso/GslNumericVector.h>
 #include <queso/GslMatrix.h>
 #include <queso/GslSparseMatrix.h>
+#include <libmesh/eigen_sparse_vector.h>
+#include <libmesh/eigen_sparse_matrix.h>
 
 namespace QUESO {
 
@@ -247,5 +249,6 @@ ConcatenatedJointPdf<V,M>::computeLogOfNormalizationFactor(unsigned int numSampl
 
 template class ConcatenatedJointPdf<GslVector, GslMatrix>;
 template class ConcatenatedJointPdf<GslNumericVector<libMesh::Number>, GslSparseMatrix<libMesh::Number> >;
+template class ConcatenatedJointPdf<libMesh::EigenSparseVector<libMesh::Number>, libMesh::EigenSparseMatrix<libMesh::Number> >;
 
 }  // End namespace QUESO
