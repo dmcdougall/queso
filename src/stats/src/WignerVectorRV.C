25478e1d967250e4dbeecadf6c057babf8731c1e
Damon McDougall <damon@ices.utexas.edu>
2018-10-23 20:51:06 -0500

    Add explicit instantiation around eigen types

--- src/stats/src/WignerVectorRV.C
+++ src/stats/src/WignerVectorRV.C
@@ -29,6 +29,8 @@
 #include <queso/GslNumericVector.h>
 #include <queso/GslMatrix.h>
 #include <queso/GslSparseMatrix.h>
+#include <libmesh/eigen_sparse_vector.h>
+#include <libmesh/eigen_sparse_matrix.h>
 
 namespace QUESO {
 
@@ -89,5 +91,6 @@ WignerVectorRV<V,M>::print(std::ostream& os) const
 
 template class WignerVectorRV<GslVector,GslMatrix>;
 template class WignerVectorRV<GslNumericVector<libMesh::Number>,GslSparseMatrix<libMesh::Number> >;
+template class WignerVectorRV<libMesh::EigenSparseVector<libMesh::Number>,libMesh::EigenSparseMatrix<libMesh::Number> >;
 
 }  // End namespace QUESO
