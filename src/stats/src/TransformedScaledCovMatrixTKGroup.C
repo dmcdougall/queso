88fc043d7ef02106c84bf400b70e9b0b0400f384
Damon McDougall <damon@ices.utexas.edu>
2018-10-28 13:22:29 -0500

    Add explicit instantiation around eigen types

--- src/stats/src/TransformedScaledCovMatrixTKGroup.C
+++ src/stats/src/TransformedScaledCovMatrixTKGroup.C
@@ -28,6 +28,8 @@
 #include <queso/GslNumericVector.h>
 #include <queso/GslMatrix.h>
 #include <queso/GslSparseMatrix.h>
+#include <libmesh/eigen_sparse_vector.h>
+#include <libmesh/eigen_sparse_matrix.h>
 
 namespace QUESO {
 
@@ -363,5 +365,6 @@ TransformedScaledCovMatrixTKGroup<V, M>::transformCovMatrixToGaussianSpace(
 
 template class TransformedScaledCovMatrixTKGroup<GslVector, GslMatrix>;
 template class TransformedScaledCovMatrixTKGroup<GslNumericVector<libMesh::Number>, GslSparseMatrix<libMesh::Number> >;
+template class TransformedScaledCovMatrixTKGroup<libMesh::EigenSparseVector<libMesh::Number>, libMesh::EigenSparseMatrix<libMesh::Number> >;
 
 }  // End namespace QUESO
